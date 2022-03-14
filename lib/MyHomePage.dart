9b0941a674316b1758402e00ad33d540ee5c4eb9
lsh990129 <zx13xz@naver.com>
2022-03-14 16:14:33 +0900

    add timezone settings

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -1,9 +1,10 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-
 import 'main.dart';
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({ Key? key }) : super(key: key);
@@ -37,15 +38,36 @@ class MyHomePage extends StatelessWidget {
                             IOSFlutterLocalNotificationsPlugin>()
                         ?.requestPermissions(alert: true, badge: true, sound: true) ??
                     false;
-            print('permission $permission');
 
             if (!permission){
               return;
             }
-            
-            await flutterLocalNotificationsPlugin.show(
-              0, 'plain title', 'plain body', detail,
-              );
+
+            // await flutterLocalNotificationsPlugin.show(
+            //   0, 'plain title', 'plain body', detail,
+            //   );
+
+            // 타임존 셋팅 필요
+            final now = tz.TZDateTime.now(tz.local);
+
+            await notification.zonedSchedule(
+              1,
+              'alarmTitle',
+              'alarmDescription',
+              tz.TZDateTime(
+                tz.local,
+                now.year,
+                now.month,
+                now.day,
+                now.hour,
+                now.minute + 1,
+              ),
+              detail,
+              androidAllowWhileIdle: true, // 저전력 유휴 모드인 경우 알람 전달 여부
+              uiLocalNotificationDateInterpretation: // 예약된 날짜를 절대 시간으로 해석할지 벽시계 시간으로 해석할지 결정
+                  UILocalNotificationDateInterpretation.absoluteTime,
+              matchDateTimeComponents: DateTimeComponents.time, // 주기 설정
+            ); 
         }, child: const Text("alarm"),
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_timezone/flutter_native_timezone.dart';
 import 'MyHomePage.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,6 +15,10 @@ void main() {
 final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
 void _initNotiSetting() async {
+  tz.initializeTimeZones();
+  final timeZoneName = await FlutterNativeTimezone.getLocalTimezone(); 
+  tz.setLocalLocation(tz.getLocation(timeZoneName));
+
   const initializationSettingsAndroid =
       AndroidInitializationSettings('@mipmap/ic_launcher');
 
